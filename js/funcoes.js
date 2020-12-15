44d2f7de3b1f1c11c6cc053d5d25774c864fd96f
LeticiaCustodio <leticiaacustodio@gmail.com>
2020-12-14 23:09:07 -0300

    Site Playstation

---
+++ js/funcoes.js
@@ -0,0 +1,11 @@
+function TrocarImagem(imagemNova){
+  // document.querySelector(".starbucks").src = imagemNova;
+  let copo = document.querySelector(".playstation");
+  copo.src = imagemNova;
+}
+
+function MudarCirculo(corNova){
+    let circulo = document.querySelector(".circulo");
+    circulo.style.background = corNova;
+
+}
